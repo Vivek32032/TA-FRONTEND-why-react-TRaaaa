6e9ddb8c81de55f367629405ce4caade028d2537
Vivek Kumar <vivek32032@gmail.com>
2022-07-03 01:32:20 +0530

    Short-news App

---
+++ block-BRaaao/news-app/src/components/App.js
@@ -0,0 +1,18 @@
+import Articles from "./Articles";
+import Header from "./Header";
+import Hero from "./Hero";
+import Footer from "./Footer";
+
+
+
+function App(props) {
+    return (
+        <>
+        <Header />
+        <Hero />
+        <Articles />
+        <Footer />
+        </>
+    )
+}
+export default App;
---
+++ block-BRaaao/news-app/src/components/Hero.js
@@ -0,0 +1,13 @@
+function Hero() {
+    return (
+        <>
+        <div className="flex mt-10 rounded">
+            <div className="w=full">
+                <img src="/images/articles.jpg" alt="Article" className="w-full rounded"/>
+            </div>
+        </div>
+
+        </>
+    )
+}
+export default Hero;
